5b4369ce8dae02c6056263df41e5b0ef2626c29f
irl231 <91590512+lazuee@users.noreply.github.com>
2026-03-01 12:34:34 +0800

    refactor: replace randomUUID with generateNonce
    - for better compatibility and remove unused polyfill

--- src/polyfills/crypto.ts
+++
@@ -1,5 +0,0 @@
-export const randomUUID = (): string => {
-	return '00000000-0000-0000-0000-000000000000'.replace(/0/g, () => {
-		return Math.floor(Math.random() * 16).toString(16);
-	});
-};
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,5 @@
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'tsdown/config';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 const config = defineConfig([
 	{
 		entry: 'src/index.ts',
@@ -12,11 +9,8 @@ const config = defineConfig([
 		dts: false,
 		exports: true,
 		fixedExtension: true,
-		minify: true,
+    minify: true,
 		shims: true,
-		alias: {
-			crypto: resolve(__dirname, './src/polyfills/crypto.ts'),
-		},
 	},
 	{
 		entry: 'src/index.ts',
@@ -44,7 +38,7 @@ const config = defineConfig([
 					}),
 				),
 		},
-		minify: true,
+    minify: true,
 		fixedExtension: true,
 	},
 ]);
